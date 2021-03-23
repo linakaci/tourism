6cb5e180d8e601d2ce299ce7fc6688e541c6e50f
walid khirdine <khirdine.walid@gmail.com>
2021-03-23 06:37:57 +0100

    ADD: connection to back

--- client/Components/PlacesList.js
+++ client/Components/PlacesList.js
@@ -1,22 +1,80 @@
 import React from 'react'
-import {StyleSheet,View,FlatList} from 'react-native'
+import {StyleSheet,View,FlatList,Text} from 'react-native'
 import Data from '../Helpers/Data'
+import { getPlaces } from '../Helpers/API'
+
 import Visiter from './Visiter'
 
 class PlacesList extends React.Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            places: [],
+          };
+      
+      }
+
+    componentDidMount() {
+      let myHeaders = new Headers();
+      let GetInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+      fetch( 'http://192.168.1.110:5000/api/places', GetInit )
+          .then(res => res.json()).then((result) => { 
+          let data = JSON.stringify(result.places)
+          this.setState({
+          isLoaded: true,
+          places: JSON.parse(data)
+        })
+        }).catch (err => {
+          console.error(err)
+        })
+    
+      }
+    
+
+      _displayLoading() {
+        if (this.state.isLoaded) {
+          return (
+            <View style={styles.main_container}>
+              <Text>en cours de chargement</Text>
+            </View>
+          )
+        }
+      }
+      
+      _displayPlaces() {
+       
+        if (this.state.isLoaded == true) {
+          return (
+            <View style={styles.main_container}>
+              <FlatList
+                    data={this.state.places}
+                    keyExtractor= {(item) => item.id.toString()}
+                    renderItem= {({item}) => <Visiter places={item}/>}
+                    />
+            </View>
+          )
+        }
+      }
+
 
     render(){
-        return(
-            <View style={styles.main_container}>
-                <FlatList
-                data={Data}
-                keyExtractor= {(item) => item.id.toString()}
-                renderItem= {({item}) => <Visiter places={item}/>}
-                />
-            </View>
+      
+        const { error, isLoaded, items } = this.state;
+        return (
+          <View style={styles.main_container}>
+            {this._displayPlaces()}
+          </View>
         )
     }
-}
+  }
+
 
 const styles = StyleSheet.create({
 
--- client/Components/Visiter.js
+++ client/Components/Visiter.js
@@ -4,15 +4,17 @@ import { Text, View, Image, StyleSheet,FlatList } from 'react-native';
 export default class Visiter extends React.Component {
   render() {
     const places = this.props.places
+
     return (
+
       <View style={styles.main_container}>
         <Image
           style={styles.image}
-          source={{ uri: places.image_uri }}
+          source={{uri:places.image}} // verifier la sytaxe pour url/uri afin de charger les images depuis les assets
         />
         <View style={styles.content_container}>
           <View style={styles.header_container}>
-            <Text style={styles.title_text}>{places.title}</Text>
+            <Text style={styles.title_text}>{places.nom}</Text>
           </View>
           <View style={styles.description_container}>
             <Text style={styles.description_text} numberOfLines={6}>{places.description}</Text>
